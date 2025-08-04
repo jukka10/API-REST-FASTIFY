708bf426121a5409ff7107207532cd7e56e79a89
jukka10 <jukka4996@gmail.com>
2025-08-04 11:03:29 -0300

    add environment variables

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,19 @@
+import "dotenv/config";
 import { knex as setupKnex, Knex } from "knex";
+
+if (!process.env.DATABASE_URL || !process.env.DIRECTORY_MIGRATIONS) {
+  throw new Error("env not found");
+}
 
 export const config: Knex.Config = {
   client: "sqlite3",
   connection: {
-    filename: "./src/temp/app.db",
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
     extension: "ts",
-    directory: "./src/temp/migrations",
+    directory: process.env.DIRECTORY_MIGRATIONS,
   },
 };
 
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import { knex } from "./database";
 const app = fastify();
 
 app.get("/", async () => {
-  const tables = await knex("sqlite_schema").select("*");
-  return tables;
+  const transaction = await knex("transactions").select("*");
+  return transaction;
 });
 
 app.listen({ port: 3333 }).then(() => {
