6944f4626ef72ad23951ed1310bbdc257fc8723b
jukka10 <jukka4996@gmail.com>
2025-08-11 09:44:16 -0300

    config production

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { app } from "./app";
 import { env } from "./env/index";
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log("hellow my opa");
-});
+app
+  .listen({
+    port: env.PORT,
+    host: "RENDER" in process.env ? "0.0.0.0" : "localhost",
+  })
+  .then(() => {
+    console.log("hellow, HTTP Server Runing!!");
+  });
